863f8f69cba8ca2d042b1a69e22a231e4ce244d8
Nicolai Schou <nisc0003@stud.kea.dk>
2026-05-21 12:43:30 +0200

    fix/header_footer_layout_bugs

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,11 +1,7 @@
-// .prettierrc.mjs
+// Prettier configuration
 /** @type {import("prettier").Config} */
 export default {
-<<<<<<<< HEAD:prettier.config.mjs
-  plugins: ["prettier-plugin-astro"],
-========
   plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
->>>>>>>> feature/Tailwind.css:.prettierrc.mjs
   overrides: [
     {
       files: "*.astro",
