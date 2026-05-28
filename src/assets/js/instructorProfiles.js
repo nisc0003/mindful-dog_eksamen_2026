f0017e1500fd368c5c7fabd390b420c64c6fd5de
Nicolai Schou <nisc0003@stud.kea.dk>
2026-05-28 18:38:15 +0200

    fix_layout

---
+++ src/assets/js/instructorProfiles.js
@@ -0,0 +1,17 @@
+import alexandra from "../testimages/alexandra.png";
+import eva from "../testimages/eva.png";
+import freja from "../testimages/freja.png";
+import grace from "../testimages/grace.png";
+import pia from "../testimages/pia.png";
+import sabine from "../testimages/sabine.png";
+import isabell from "../testimages/isabell.png";
+
+export const instructorProfiles = [
+	{ slug: "alexandra", name: "Alexandra", location: "Nordsjælland", image: alexandra },
+	{ slug: "eva", name: "Eva", location: "København", image: eva },
+	{ slug: "freja", name: "Freja", location: "Midtsjælland", image: freja },
+	{ slug: "grace-hidalgo", name: "Grace", location: "Hovedkontor", image: grace },
+	{ slug: "pia", name: "Pia", location: "Fyn", image: pia },
+	{ slug: "sabine", name: "Sabine", location: "Sydsjælland", image: sabine },
+	{ slug: "isabell", name: "Isabell", location: "Vestjylland", image: isabell },
+];
