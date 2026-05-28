9ae45e707804d7e411a08ca1e9b7ce68c0e86cb2
Nicolai Schou <nisc0003@stud.kea.dk>
2026-05-28 18:38:21 +0200

    sync

--- src/assets/js/instructorProfiles.js
+++ src/assets/js/instructorProfiles.js
@@ -7,11 +7,21 @@ import sabine from "../testimages/sabine.png";
 import isabell from "../testimages/isabell.png";
 
 export const instructorProfiles = [
-	{ slug: "alexandra", name: "Alexandra", location: "Nordsjælland", image: alexandra },
-	{ slug: "eva", name: "Eva", location: "København", image: eva },
-	{ slug: "freja", name: "Freja", location: "Midtsjælland", image: freja },
-	{ slug: "grace-hidalgo", name: "Grace", location: "Hovedkontor", image: grace },
-	{ slug: "pia", name: "Pia", location: "Fyn", image: pia },
-	{ slug: "sabine", name: "Sabine", location: "Sydsjælland", image: sabine },
-	{ slug: "isabell", name: "Isabell", location: "Vestjylland", image: isabell },
+  {
+    slug: "alexandra",
+    name: "Alexandra",
+    location: "Nordsjælland",
+    image: alexandra,
+  },
+  { slug: "eva", name: "Eva", location: "København", image: eva },
+  { slug: "freja", name: "Freja", location: "Midtsjælland", image: freja },
+  {
+    slug: "grace-hidalgo",
+    name: "Grace",
+    location: "Hovedkontor",
+    image: grace,
+  },
+  { slug: "pia", name: "Pia", location: "Fyn", image: pia },
+  { slug: "sabine", name: "Sabine", location: "Sydsjælland", image: sabine },
+  { slug: "isabell", name: "Isabell", location: "Vestjylland", image: isabell },
 ];
